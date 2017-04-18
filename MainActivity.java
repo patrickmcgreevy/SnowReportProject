e3c57560c2e0d80d1c3f297cca25fc05c9156ba6
ros_pjmcgreevy <shebamaster9810510>
2017-04-18 09:33:22 -0700

    4/18/17

--- MainActivity.java
+++ MainActivity.java
@@ -2,12 +2,23 @@ package com.example.ros_pjmcgreevy.snowreport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView alpentalSnowData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        alpentalSnowData = (TextView) findViewById(R.id.snow_text_view);
+
+        String[] alpentalWeather = {
+            "Aplental", "22", "26", "100"
+        };
+    }
+
+    public void printSnowData() {
+
     }
 }
