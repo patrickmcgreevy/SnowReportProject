6ddbfea187dc51346d93744743a18decefdde6dd
ros_pjmcgreevy <shebamaster9810510>
2017-04-24 09:36:10 -0700

    button works, working on setButton Method now

--- MainActivity.java
+++ MainActivity.java
@@ -9,34 +9,40 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private TextView mainText;
     private Button alpButton;
+    private TextView text2;
+    private Button crystalButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mainText = (TextView) findViewById(R.id.snow_text_view);
-
-        String[] alpentalWeather = {
-                "Alpental", "Low: 22", "High: 26", "Base: 100"
-        };
-
+        text2 = (TextView) findViewById(R.id.text2);
+        final resort alpental = new resort("Alpental", 33, 26, "Cloudy", 20);
         alpButton = (Button) findViewById(R.id.alp_button);
+
+        //sets the button's onclick function. Runs the printSnowData() method
         alpButton.setOnClickListener(new View.OnClickListener() {
            public void onClick(View v) {
-               printSnowData(alpentalWeather);
-               mainText.append("I work");
+               printSnowData(alpental.getDataArray());
            }
         });
-
-        printSnowData(alpentalWeather);
+        //uncomment to work on making the setButton method work. Uncommenting breaks code.
+        //setButton(crystalButton, alpental);
     }
 
-    //Adds the text of the snow data to the TextView
+    //Adds the text of the snow data to the TextView by taking the resort.getDataArray() method as a parameter
     public void printSnowData(String[] snowData) {
         for(String i : snowData) {
-            mainText.append(i + "  ");
+            mainText.append(i + "\n");
         }
     }
 
-
+    public void setButton(Button button, final resort resort) {
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                printSnowData(resort.getDataArray());
+            }
+        });
+    }
 }
---
+++ resort.java
@@ -0,0 +1,53 @@
+package com.example.ros_pjmcgreevy.snowreport;
+
+/**
+ * Created by ros_pjmcgreevy on 4/21/2017.
+ */
+
+public class resort {
+    private String resName;
+    private int lowTemp;
+    private int highTemp;
+    private String weather;
+    private int snowDepth;
+
+    public resort(String resName, int lowTemp, int highTemp, String weather, int snowDepth) {
+        this.resName = resName;
+        this.lowTemp = lowTemp;
+        this.highTemp =  highTemp;
+        this.weather = weather;
+        this.snowDepth = snowDepth;
+    }
+
+    public String getResName() {
+        return resName;
+    }
+
+    public int getLowTemp() {
+        return lowTemp;
+    }
+
+    public int getHighTemp() {
+        return highTemp;
+    }
+
+    public String getWeather() {
+        return weather;
+    }
+
+    public int getSnowDepth() {
+        return snowDepth;
+    }
+    //returns the data of the resort as a string broken by new lines after each pice of data
+    public String getData() {
+        String data = lowTemp + "\n" + highTemp + "\n" + weather + "\n" + snowDepth;
+        return data;
+    }
+    //returns the data of the resort as a String array
+    public String[] getDataArray() {
+        String[] data = {
+                "Alpental", "Low: 22", "High: 26", "Base: 100"
+        };
+        return data;
+    }
+}
